82e7dccbb6bf917a94b9531c2080a3bdd259f4fb
Shragei <shragei@gmail.com>
2015-11-08 19:56:03 -0800

    Add tool to handle packet capture to db

---
+++ Tools/capture2db.js
@@ -0,0 +1,50 @@
+//requires root to use pcap
+var crypto=require('crypto');
+var mongojs=require('mongojs')
+var Capture=require('../lib/net/Capture');
+var Decoder=require('../lib/net/Decoder');
+var config=require('../config.json');
+
+var mongojs=require('mongojs');
+var db=mongojs(config.dburl,['Session','Packets','Messages']);
+
+var sessionId=crypto.randomBytes(6).toString('base64');console.log(sessionId)
+if(process.argv.length===3){ //override default sessionId with user provided sessionId
+  sessionId=process.argv[2];
+}
+console.log(sessionId);
+
+db.Session.createIndex({SessionId:1});
+db.Packets.createIndex({SessionId:1});
+db.Packets.createIndex({Timestamp:1});
+
+var cap=new Capture(config.TargetMAC);
+var decoder=new Decoder();
+var startTime=Date.now()/1000;
+
+cap.pipe(decoder);
+
+var packetCount=0;
+var totalData=0;
+decoder.on('data',function(data){
+  if(data.Frame.IP!==undefined&&data.Frame.IP.UDP!==undefined&&data.Frame.IP.UDP.NinFrame!==undefined){//ignore all packets that don't have have the ninFrame
+    packetCount++;
+    totalData+=data.Frame.IP.UDP.Payload.length
+    db.Packets.save(data);
+  }
+});
+
+process.on('SIGINT',function(){console.log('here');
+  var endtime=Date.now()/1000
+  db.Session.save({
+    SessionId:sessionId,
+    StartStart:startTime,
+    EndTime:endtime,
+    Duration:endtime-startTime,
+    PacketCount:packetCount,
+    TotalData:totalData
+  });
+  console.log("Session: "+sessionId+" Duration: "+(endtime-startTime));
+  process.exit();
+});
+cap.Start(sessionId);
